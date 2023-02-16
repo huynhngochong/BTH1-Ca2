2f19c19dfa5982673dc5aac22ab75bbac82fef59
ngoc hong <1751012027hong@ou.edu.vn>
2023-02-16 17:14:44 +0700

    Cap nhat cho nhanh moi

--- app/src/main/java/com/mycompany/app/App.java
+++ app/src/main/java/com/mycompany/app/App.java
@@ -13,5 +13,9 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int x = 10;
+        
+        if(x >= 10)
+            return;
     }
 }
